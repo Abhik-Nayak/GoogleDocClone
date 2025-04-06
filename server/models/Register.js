fb358c302e39456e43c94dfff05937c86ff58565
Abhik Nayak <abhiknayakabhik@gmail.com>
2025-04-06 07:56:09 +0530

    code push

---
+++ server/models/Register.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const RegisterSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const RegisterModel = mongoose.model("register", RegisterSchema);
+
+export default RegisterModel;
