0aecb969fd6ab0d071ec0757eb13829ce2dac6af
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-09-08 11:32:44 +0530

    Remove crush-the-candy app and add user service setup
    Deleted the entire crush-the-candy React app, including all source files, configuration, and assets. Added initial package.json and tsconfig.json for a new user service under services/user.

--- crush-the-candy/src/store/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "./index.ts";
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- crush-the-candy/src/utils/formula.ts
+++
@@ -1,5 +0,0 @@
-export  const formulaForColumnOfFour = (boadSize: number) =>
-    boadSize * boadSize - (boadSize + boadSize + boadSize) - 1;
-
-export  const formulaForColumnOfThree = (boadSize: number) =>
-    boadSize * boadSize - (boadSize + boadSize) - 1;
